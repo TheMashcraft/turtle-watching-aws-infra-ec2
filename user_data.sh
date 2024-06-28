efd07b98896a4602ace15b829d073d9ce2849a81
Matt Ashcraft <ashcraft39@gmail.com>
2024-06-27 23:43:13 -0400

    Add updates to user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+# Update the instance
 sudo yum update -y
+
+# Install Python 3 and Git
 sudo yum install -y python3 git
 
+# Install pip for Python 3
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
+sudo python3 get-pip.py
 
-pip3 install flask
+# Install Flask using pip
+sudo pip3 install flask
 
 # Clone the Flask app from your repository
 cd /home/ec2-user
 git clone https://github.com/TheMashcraft/turtle-watching.git
 cd turtle-watching
-pip3 install -r requirements.txt
+
+# Install the required Python packages
+sudo pip3 install -r requirements.txt
 
 # Run the Flask app
 nohup python3 app.py &
