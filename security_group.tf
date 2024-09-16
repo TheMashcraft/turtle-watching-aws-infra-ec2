9ff57ccc1244b3d69131cbdb806e0f30f01d823d
Matt Ashcraft <matthew.ashcraft@slalom.com>
2024-09-16 12:19:32 -0400

    Update SG port for EC2

--- security_group.tf
+++ security_group.tf
@@ -11,8 +11,8 @@ resource "aws_security_group" "flask_sg" {
   }
 
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
